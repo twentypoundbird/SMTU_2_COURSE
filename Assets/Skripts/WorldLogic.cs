86736c9e1ec6ce1a2bbb63d3055ddcd0f031ad0d
Kompreso <22012001_23@list.ru>
2020-12-01 17:05:53 +0300

    Логику классную сделал с передвижением, и массивом.

--- Assets/Skripts/WorldLogic.cs
+++ Assets/Skripts/WorldLogic.cs
@@ -8,11 +8,10 @@ using System;
 
 public class WorldLogic : MonoBehaviour
 {
-
     public GameObject submarine, mine, chainLink, fixOnTheGround;
 
-
     private GameObject model3D;
+    private int modelID;
 
     private static int step = MapSizeEditor.step;
 
@@ -21,18 +20,29 @@ public class WorldLogic : MonoBehaviour
     public Material generalMaterial, invisibleMaterial,
         sandMaterial;
     public Mesh generalMesh;
-    
+
     public static Material materialForMiniCube;
 
     private GameObject miniCube, newMiniCube;
     private BoxCollider boxOfMinicube;
 
+    GameObject[,,] TypeOfObjectOnMap;
 
     float mouseScrollValue;
     float posX, posY, posZ;
 
+    public Color cl_warning;
+    private Color cl_temp;
+    private float positionX, positionY, positionZ, differenceX, differenceY, differenceZ;
+    int xCoord,yCoord,zCoord;
+
+    WorldLogic()
+    {
+        TypeOfObjectOnMap = new GameObject[MapSizeEditor.countX, MapSizeEditor.countY, MapSizeEditor.countZ];
+    }
     public void IsTapped(int num)
     {
+        modelID = num;
         switch (num)
         {
             case 1:
@@ -47,19 +57,10 @@ public class WorldLogic : MonoBehaviour
             case 4:
                 model3D = fixOnTheGround;
                 break;
-            case 5:
-                model3D = null;
-                break;
-            case 6:
-                model3D = null;
-                break;
-            case 7:
-                model3D = null;
-                break;
             default:
+                modelID = 0;
                 model3D = null;
                 break;
-
         }
     }
 
@@ -67,16 +68,15 @@ public class WorldLogic : MonoBehaviour
     {
         gameObject.transform.localScale = new Vector3(MapSizeEditor.countX * step, MapSizeEditor.countY * step, MapSizeEditor.countZ * step);
 
-
         for(int x = 0; x < MapSizeEditor.countX; x++)
         {
             for (int y = 0; /*y < MapSizeEditor.sizeY / step*/ y<1; y++)
             {
                 for (int z = 0; z < MapSizeEditor.countZ; z++)
                 {
-                    int xCoord = (int)(step * (x/* - MapSizeEditor.countX / 2 + 0.5*/));
-                    int yCoord = (int)(step * (y/* - MapSizeEditor.countY / 2 + 0.5*/));
-                    int zCoord = (int)(step * (z/* - MapSizeEditor.countZ / 2 + 0.5*/));
+                    xCoord = (int)(step * (x/* - MapSizeEditor.countX / 2 + 0.5*/));
+                    yCoord = (int)(step * (y/* - MapSizeEditor.countY / 2 + 0.5*/));
+                    zCoord = (int)(step * (z/* - MapSizeEditor.countZ / 2 + 0.5*/));
                     miniCube = new GameObject();
                     miniCube.transform.position = new Vector3(xCoord, yCoord, zCoord);
                     miniCube.transform.localScale = new Vector3(step, step, step);
@@ -106,7 +106,7 @@ public class WorldLogic : MonoBehaviour
                 if (model3D != null)
                 {
                     newMiniCube = new GameObject();
-                    newMiniCube.transform.localScale = new Vector3(step, step, step);
+                    newMiniCube.transform.localScale = new Vector3(step + 1, step +1 , step + 1);
                     newMiniCube.transform.parent = transform;
                     newMiniCube.AddComponent<MeshFilter>().mesh = generalMesh;
 
@@ -126,9 +126,18 @@ public class WorldLogic : MonoBehaviour
             }
             else
             {
-                newMiniCube.AddComponent<BoxCollider>().center = new Vector3(0, 0, 0);
-                newMiniCube = null;
-                mouseScrollValue = 0;
+                if (TypeOfObjectOnMap[xCoord, yCoord, zCoord] == null)
+                {
+                    newMiniCube.transform.localScale = new Vector3(step / 100f, step / 100f, step / 100f);
+                    newMiniCube.AddComponent<BoxCollider>().center = new Vector3(0, 0, 0);
+                    TypeOfObjectOnMap[xCoord, yCoord, zCoord] = newMiniCube;
+                    newMiniCube = null;
+                    mouseScrollValue = 0;
+                }
+                else
+                {
+                    Debug.LogWarning("Место занято другим объектом!");
+                }
             }
         }
 
@@ -143,7 +152,6 @@ public class WorldLogic : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 10000))
             {
-                float positionX, positionY, positionZ, differenceX, differenceY, differenceZ;
                 differenceX = Mathf.Abs(hit.point.x - hit.collider.gameObject.transform.position.x) < step / 2 - 0.0001f ? 0 : hit.point.x - hit.collider.gameObject.transform.position.x;
                 differenceY = Mathf.Abs(hit.point.y - hit.collider.gameObject.transform.position.y) < step / 2 - 0.0001f ? 0 : hit.point.y - hit.collider.gameObject.transform.position.y;
                 differenceZ = Mathf.Abs(hit.point.z - hit.collider.gameObject.transform.position.z) < step / 2 - 0.0001f ? 0 : hit.point.z - hit.collider.gameObject.transform.position.z;
@@ -152,22 +160,46 @@ public class WorldLogic : MonoBehaviour
                 positionY = hit.collider.gameObject.transform.position.y + differenceY * 2 + mouseScrollValue * step;
                 positionZ = hit.collider.gameObject.transform.position.z + differenceZ * 2;
 
-                int xCoord = (int)((positionX / step) - 0.5 + MapSizeEditor.countX / 2);
-                int yCoord = (int)((positionY / step) - 0.5 + MapSizeEditor.countY / 2);
-                int zCoord = (int)((positionZ / step) - 0.5 + MapSizeEditor.countZ / 2);
+                xCoord = (int)(positionX / step + 0.1);
+                yCoord = (int)(positionY / step + 0.1);
+                zCoord = (int)(positionZ / step + 0.1);
 
                 Debug.Log("Cord{" + positionX + ":" + positionY + ":" + positionZ + "}");
                 Debug.Log("RealCord{" + xCoord + ":" + yCoord + ":" + zCoord + "}");
-                
 
-                newMiniCube.transform.position = new Vector3(positionX, positionY, positionZ);
 
-                if(model3D == mine)
+                if (positionX >= 0 && xCoord < MapSizeEditor.countX)
                 {
-                    RaycastHit hitUnderMine;
-                    if (Physics.Raycast(newMiniCube.transform.position, -1 * newMiniCube.transform.up, out hitUnderMine, 100 * step))
+                    if (positionY >= 0 && yCoord < MapSizeEditor.countY)
                     {
-                        
+                        if (positionZ >= 0 && zCoord < MapSizeEditor.countZ)
+                        {
+                            newMiniCube.transform.position = new Vector3(positionX, positionY, positionZ);
+                            if (TypeOfObjectOnMap[xCoord, yCoord, zCoord] == null)
+                            {
+                                if (cl_temp != newMiniCube.GetComponentInChildren<MeshRenderer>().sharedMaterial.color)
+                                {
+                                    if (cl_warning != newMiniCube.GetComponentInChildren<MeshRenderer>().sharedMaterial.color)
+                                    {
+                                        cl_temp = newMiniCube.GetComponentInChildren<MeshRenderer>().sharedMaterial.color;
+                                    }
+                                }
+                                newMiniCube.GetComponentInChildren<MeshRenderer>().sharedMaterial.color = cl_temp;
+                            }
+                            else
+                            {
+                                newMiniCube.GetComponentInChildren<MeshRenderer>().sharedMaterial.color = cl_warning;
+                                newMiniCube.GetComponentInChildren<MeshRenderer>().sharedMaterial.color = cl_warning;
+                            }
+                            if (model3D == mine)
+                            {
+                                RaycastHit hitUnderMine;
+                                if (Physics.Raycast(newMiniCube.transform.position, -1 * newMiniCube.transform.up, out hitUnderMine, 100 * step))
+                                {
+
+                                }
+                            }
+                        }
                     }
                 }
             }
